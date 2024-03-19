293123b7761b08b077ee19f10d40ca6c9fca87cb
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-03-19 10:56:21 +0100

    chalk

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+
+
+const collegues = ["Joris", "Jean-bernard", "Adeline", "charlot"];
+
+
+console.log(chalk.blue(collegues[0]));
+console.log(chalk.red(collegues[1]));
+console.log(chalk.green(collegues[2]));
+console.log(chalk.yellow(collegues[3]));
